45763f604cb53a04fab095bb5feea9a831d04de3
Fra <frawolf@outlook.it>
2020-12-17 18:43:20 +0100

    Implemented transfer tokens function

--- contracts/WolfToken.sol
+++ contracts/WolfToken.sol
@@ -1,18 +1,68 @@
 pragma solidity ^0.5.0;
 
 contract WolfToken {
-
-    // Contructor, it runs when the Smart Contract gets deployed
-    // Set the total number of tokens
-    // Read the total number of tokens
+    
+    //-----------------
+    // Token informations
+    //-----------------
+    
+    string public name      = "Wolf Token";
+    string public symbol    = "WOLF";
+    string public version   = "1.0";
 
     uint256 public totalSupply;
 
-    constructor () public {
+    // Array for user's balances
+    mapping(address => uint256) public balanceOf;
+
+    //-----------------
+    // Events
+    //-----------------
+
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 _value
+    );
+
+    //-----------------
+    // Constructor
+    //-----------------
+
+    constructor (uint256 _initialSupply) public {
+
+        // Assign the initial supply to the Token Owner
+        balanceOf[msg.sender] = _initialSupply;
 
         // Total tokens available
-        totalSupply = 1000000; // 1M Tokens
+        totalSupply = _initialSupply;
+
+    }
+
+    //-----------------
+    // Functions
+    //-----------------
+
+    // Transfer function (to pay with it)
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+
+        // Check if the address has enough token to transfer
+        require(balanceOf[msg.sender] >= _value);
+
+        // Remove tokens from the sender
+        balanceOf[msg.sender] -= _value;
+
+        // Add tokens to the receiver
+        balanceOf[_to] += _value;
+
+        // Emitting the transfer event
+        emit Transfer(msg.sender, _to, _value);
+
+        // Return a boolean
+        return true;
 
     }
 
 }
+
+// require is an sort of if statement
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 const WolfToken = artifacts.require("./WolfToken.sol");
 
 module.exports = function(deployer) {
-    deployer.deploy(WolfToken);
+    
+    // Arguments will be passed into the constructor
+    deployer.deploy(WolfToken, 1000000);
 };
